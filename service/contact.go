a388b5a765640f0a825030fa679fb7c6fcf268f3
Ivan Bobynets <ivan@osyah.com>
2025-04-08 21:50:41 +0300

    service: add Contact and NewContact

---
+++ service/contact.go
@@ -0,0 +1,45 @@
+// Copyright (c) 2025 Osyah
+// SPDX-License-Identifier: MIT
+
+package service
+
+import (
+	"github.com/charmbracelet/bubbles/list"
+
+	"github.com/osyah/homin"
+	"github.com/osyah/homin/config"
+	"github.com/osyah/homin/context"
+)
+
+type Contact struct {
+	locals map[string]config.Contact
+}
+
+func NewContact() *Contact {
+	return &Contact{locals: make(map[string]config.Contact)}
+}
+
+func (c *Contact) Get(*context.Context) ([]list.Item, error) {
+	var err error
+
+	if len(c.locals) == 0 {
+		c.locals, err = config.GetContacts()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	contacts := make([]list.Item, len(c.locals))
+
+	var i uint16
+
+	for hash, local := range c.locals {
+		contacts[i] = &homin.LocalContact{
+			Name: local.Name, Hash: hash,
+		}
+
+		i++
+	}
+
+	return contacts, nil
+}
